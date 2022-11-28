eed7ec1a026a9948b2691438fcebd1daa7eeae18
Spooghetti420 <kkovachev1947@gmail.com>
2022-11-28 19:00:32 +0000

    add some library functions

---
+++ library.scm
@@ -0,0 +1,74 @@
+(define (identity x) x)
+
+(define (square x)
+  (* x x))
+
+(define (sum-of-squares x y)
+    (+ (square x) (square y))
+)
+
+(define (abs x)
+    (if (> x 0)
+        x
+        (- x)
+    )
+)
+
+(define (mean x y)
+    (/ (+ x y) 2)
+)
+
+(define (>= x y)
+    (or (> x y) (= x y))
+)
+
+(define (<= x y)
+    (or (< x y) (= x y))
+)
+
+(define (ratio-error actual approx)
+    (/ (- approx actual) actual)
+)
+
+(define (percentage-error actual approx)
+    (* (ratio-error actual approx) 100)
+)
+
+(define (sqrt x)
+    (define (improve-guess guess)
+        (mean guess (/ x guess))
+    )
+    (define (good-enough? guess)
+        (<= (abs (percentage-error x (square guess))) 1)
+    )
+    (define (sqrt-iter guess)
+        (if (good-enough? guess)
+            guess
+            (sqrt-iter (improve-guess guess))
+        )
+    )
+    (sqrt-iter 1)
+)
+
+(define (derivative f x)
+    (let ((h 1e-3))
+        (/
+            (- 
+                (f (+ x h))
+                (f x)
+            )
+        	h
+        )
+    )
+)
+
+(define (sum start-value term-func inc-func pred)
+    (define (sum-inner current-value current-total)
+        (if (pred current-value)
+            (sum-inner (inc-func current-value) (+ (term-func current-value) current-total))
+            current-total
+        )
+    )
+    (sum-inner start-value 0)
+)
+
